5f79cce603bdb30ae402e61c4bc39e24016576e8
haroldwongms <harold.wong@microsoft.com>
2018-07-30 10:23:32 -0700

    change ref to 3.10

--- scripts/bastionPrep.sh
+++ scripts/bastionPrep.sh
@@ -57,7 +57,7 @@ subscription-manager repos --disable="*"
 subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
-    --enable="rhel-7-server-ose-3.9-rpms" \
+    --enable="rhel-7-server-ose-3.10-rpms" \
     --enable="rhel-7-server-ansible-2.4-rpms" \
     --enable="rhel-7-fast-datapath-rpms" \
     --enable="rh-gluster-3-client-for-rhel-7-server-rpms"
